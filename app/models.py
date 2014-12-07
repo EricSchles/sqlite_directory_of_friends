9f149c4874dfe052ed1d050d1b4fc61c3831b3d9
EricSchles <ericschles@gmail.com>
2014-12-07 13:22:58 -0800

    fixed insert

--- app/models.py
+++ app/models.py
@@ -4,14 +4,14 @@ import sqlite3 as sql
 def insert_account_holder(email,username,phone,password):
     con = sql.connect("database.db")
     cur = con.cursor()
-    cur.execute("INSERT INTO account_holder (email,username,phone,password) VALUES (?,?,?,?)" [email,username,phone,password])
+    cur.execute("INSERT INTO account_holder (email,username,phone,password) VALUES (?,?,?,?)", (email,username,phone,password) )
     con.commit()
     con.close()
 
 def insert_contact(name,phone,username,email):
     con = sql.connect("database.db")
     cur = con.cursor()
-    cur.execute("INSERT INTO contact (name,phone,username,email) VALUES (?,?,?,?)" [name,phone,username,email])
+    cur.execute("INSERT INTO contact (name,phone,username,email) VALUES (?,?,?,?)", (name,phone,username,email) )
     con.commit()
     con.close()
 
